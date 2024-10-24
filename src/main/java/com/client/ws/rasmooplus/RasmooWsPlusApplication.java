244f6f3d89d1da5740fb1d591541ea5d9da94d0c
wanderson648 <testepedroca@gmail.com>
2024-10-24 06:56:44 -0300

    configura projeto

---
+++ src/main/java/com/client/ws/rasmooplus/RasmooWsPlusApplication.java
@@ -0,0 +1,13 @@
+package com.client.ws.rasmooplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasmooWsPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RasmooWsPlusApplication.class, args);
+	}
+
+}
