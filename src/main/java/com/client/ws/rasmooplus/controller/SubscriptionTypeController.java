de7a02ea516cbc98e74ffa69c0e214b7d5671297
wanderson648 <testepedroca@gmail.com>
2024-10-24 08:39:19 -0300

    implementa exception handler

--- src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
+++ src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
@@ -1,10 +1,11 @@
 package com.client.ws.rasmooplus.controller;
 
 import com.client.ws.rasmooplus.model.SubscriptionType;
-import com.client.ws.rasmooplus.repository.SubscriptionTypeRepository;
+import com.client.ws.rasmooplus.service.SubscriptionTypeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,16 @@ import static org.springframework.http.HttpStatus.OK;
 @RequiredArgsConstructor
 public class SubscriptionTypeController {
 
-    private final SubscriptionTypeRepository subscriptionTypeRepository;
+    private final SubscriptionTypeService subscriptionTypeService;
 
     @GetMapping
     public ResponseEntity<List<SubscriptionType>> findAll() {
-        return ResponseEntity.status(OK).body(subscriptionTypeRepository.findAll());
+        return ResponseEntity.status(OK).body(subscriptionTypeService.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SubscriptionType> findById(@PathVariable("id") Long id) {
+        return ResponseEntity.status(OK).body(subscriptionTypeService.findById(id));
+    }
+
 }
---
+++ src/main/java/com/client/ws/rasmooplus/service/impl/SubscriptionTypeImpl.java
@@ -0,0 +1,48 @@
+package com.client.ws.rasmooplus.service.impl;
+
+import com.client.ws.rasmooplus.exception.NotFoundException;
+import com.client.ws.rasmooplus.model.SubscriptionType;
+import com.client.ws.rasmooplus.repository.SubscriptionTypeRepository;
+import com.client.ws.rasmooplus.service.SubscriptionTypeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class SubscriptionTypeImpl implements SubscriptionTypeService {
+
+
+    private final SubscriptionTypeRepository subscriptionTypeRepository;
+    @Override
+    public List<SubscriptionType> findAll() {
+        return subscriptionTypeRepository.findAll();
+    }
+
+    @Override
+    public SubscriptionType findById(Long id) {
+        Optional<SubscriptionType> optionalSubscriptionType = subscriptionTypeRepository.findById(id);
+
+        if(optionalSubscriptionType.isEmpty()) {
+            throw new NotFoundException("Subscription não encontrado");
+        }
+        return optionalSubscriptionType.get();
+    }
+
+    @Override
+    public SubscriptionType create(SubscriptionType subscriptionType) {
+        return null;
+    }
+
+    @Override
+    public SubscriptionType update(Long id, SubscriptionType subscriptionType) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
